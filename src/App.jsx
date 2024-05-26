c0c1ff416f00c5e4169d80d8d9ad66da4f6abc51
pranav <pranav09j@gmail.com>
2024-05-26 16:45:43 +0530

    react-router-project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import { useState } from 'react'
+
+import './App.css'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+
+   <>
+
+   </>
+  )
+}
+
+export default App
